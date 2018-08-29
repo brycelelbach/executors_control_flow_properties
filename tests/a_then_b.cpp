4f1fe871c8049c26450e0269c7b5a9a50e52a6f0
Jared Hoberock <jaredhoberock@gmail.com>
2018-08-29 12:00:54 -0500

    Replace use of then_execute with execute

--- tests/a_then_b.cpp
+++ tests/a_then_b.cpp
@@ -1,6 +1,6 @@
 // case 1: ExecutorB knows how to depend on ExecutorA
 template<class ExecutorA, class FunctionA, class ExecutorB, class FunctionB,
-         __REQUIRES(can_require_v<ExecutorB, depends_on_t<ExecutorA, then_t>)
+         __REQUIRES(can_require_v<ExecutorB, depends_on_t<ExecutorA, single_t>)
         >
 auto a_then_b(const ExecutorA& ex_a, FunctionA&& f_a, const ExecutorB& ex_b, FunctionB&& f_b)
 {
@@ -8,13 +8,13 @@ auto a_then_b(const ExecutorA& ex_a, FunctionA&& f_a, const ExecutorB& ex_b, Fun
 
   ex_a.execute(std::forward<FunctionA>(f_a));
 
-  return require(ex_b, depends_on(ex_a)).then_execute(std::forward<FunctionB>(f_b));
+  return require(ex_b, depends_on(ex_a)).execute(std::forward<FunctionB>(f_b));
 }
 
 // case 2: not case 1 and ExecutorB can be lazy and forgetful
 template<class ExecutorA, class FunctionA, class ExecutorB, class FunctionB,
-         __REQUIRES(!can_require_v<ExecutorB, depends_on_t<ExecutorA, then_t>),
-         __REQUIRES(can_require_v<ExecutorB, lazy_t, depends_on_t<>, then_t>)
+         __REQUIRES(!can_require_v<ExecutorB, depends_on_t<ExecutorA, single_t>),
+         __REQUIRES(can_require_v<ExecutorB, lazy_t, depends_on_t<>, single_t>)
         >
 auto a_then_b(const ExecutorA& ex_a, FunctionA&& f_a, const ExecutorB& ex_b, FunctionB&& f_b)
 {
@@ -24,7 +24,7 @@ auto a_then_b(const ExecutorA& ex_a, FunctionA&& f_a, const ExecutorB& ex_b, Fun
   auto lazy_ex_b = require(ex_b, depends_on(), lazy, then);
 
   // lazily enqueue f_b
-  auto result_ex = lazy_ex_b.then_execute(f_b);
+  auto result_ex = lazy_ex_b.execute(f_b);
 
   // execute f_a on ex_a and flush lazy_ex_b after f_a() finishes
   ex_a.execute([f_a = std::forward<FunctionA>(f_a), lazy_ex_b = std::move(lazy_ex_b)]
@@ -33,11 +33,9 @@ auto a_then_b(const ExecutorA& ex_a, FunctionA&& f_a, const ExecutorB& ex_b, Fun
     f_a();
 
     // release lazy_ex_b's work (which is f_b)
-    // XXX what does this do to result_ex?
     lazy_ex_b.flush();
   });
 
-  // XXX should we try to make result_ex's properties match ex_b's incoming properties as best we can?
   return result_ex;
 }
 
